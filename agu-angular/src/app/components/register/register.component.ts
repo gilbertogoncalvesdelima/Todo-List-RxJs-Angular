edc014b810ec677aaa90b101dac21f5d060d7dfb
gilbertogoncalvesdelima <gilberto-goncalves@outlook.com.br>
2024-07-16 15:47:57 -0300

    :lipstick: ajusta visual de register

--- agu-angular/src/app/components/register/register.component.ts
+++ agu-angular/src/app/components/register/register.component.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {AuthService} from "../../services/auth.service";
 import {Router} from "@angular/router";
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'

5dbeeee92f2c93521ba2f8b79104859270d45d40
gilbertogoncalvesdelima <gilberto-goncalves@outlook.com.br>
2024-07-16 16:21:00 -0300

    :lipstick: ajusta visual de add task

--- agu-angular/src/app/components/task/task.component.ts
+++ agu-angular/src/app/components/task/task.component.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 import {TaskService} from "../../services/task.service";
 import {FormsModule} from "@angular/forms";
 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+
 @Component({
   selector: 'app-task',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule
   ],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss'
